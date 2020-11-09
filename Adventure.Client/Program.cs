8b435c33fe57a010c101de9970ccdc4463190abe
Gino Messmer <git@ginomessmer.me>
2020-11-09 14:56:06 +0100

    Add project files.

---
+++ Adventure.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Adventure.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Adventure.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Adventure.Core
+{
+    public class Class1
+    {
+    }
+}
