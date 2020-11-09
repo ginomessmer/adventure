1a7afab5c1b47e479ad7477790943d14b3a614e9
Gino Messmer <git@ginomessmer.me>
2020-11-09 15:56:30 +0100

    Add basic sample for send/receive socket data

--- Adventure.Client/Program.cs
+++ Adventure.Client/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Adventure.Client
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create socket
+            Console.WriteLine("Connecting to socket...");
+            var entry = await Dns.GetHostEntryAsync("127.0.0.1");
+            var endpoint = new IPEndPoint(entry.AddressList.First(), 14500);
+            var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect
+            Console.WriteLine("CONNECT");
+            await socket.ConnectAsync(endpoint);
+
+            // Send
+            Console.WriteLine("SEND");
+            socket.Send(Encoding.ASCII.GetBytes("Hello world <EOF>"));
+
+            // Receive
+            var buffer = new byte[1024];
+            socket.Receive(buffer);
+            
+            Console.WriteLine("Reply: {0}", Encoding.ASCII.GetString(buffer));
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+            socket.Dispose();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
         }
     }
 }
--- Adventure.Server/Program.cs
+++ Adventure.Server/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Adventure.Server
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create socket
+            Console.WriteLine("Opening socket...");
+            var entry = await Dns.GetHostEntryAsync("127.0.0.1");
+            var endpoint = new IPEndPoint(entry.AddressList.First(), 14500);
+            var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind
+            Console.WriteLine("BIND");
+            socket.Bind(endpoint);
+
+            // Listen
+            Console.WriteLine("LISTEN");
+            socket.Listen(int.MaxValue);
+
+            // Accept
+            Console.WriteLine("ACCEPT");
+            var receiveSocket = socket.Accept();
+
+            var buffer = new byte[1024];
+            var data = "";
+
+            // Receive
+            Console.WriteLine("RECEIVE...");
+            while (true)
+            {
+                receiveSocket.Receive(buffer);
+                data += Encoding.ASCII.GetString(buffer);
+
+                if (data.IndexOf("<EOF>", StringComparison.Ordinal) > -1)
+                {
+                    receiveSocket.Send(Encoding.ASCII.GetBytes("Great success"));
+                    break;
+                }
+            }
+
+            Console.WriteLine("Data received:\n{0}", data);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
         }
     }
 }
