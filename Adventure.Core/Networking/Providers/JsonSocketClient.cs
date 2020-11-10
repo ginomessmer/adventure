e4eabd7037d516ff768de2d90cfc5c238ac8b6f6
Gino Messmer <git@ginomessmer.me>
2020-11-10 15:48:49 +0100

    Add JSON socket stubs

---
+++ Adventure.Core/Networking/Providers/JsonSocketClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Adventure.Core.Networking.Abstractions;
+
+namespace Adventure.Core.Networking.Providers
+{
+    public sealed class JsonSocketClient : SocketClient
+    {
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override event EventHandler<MessageReceivedArgs> OnMessageReceived;
+        public override void SendInitialMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Adventure.Core/Networking/Providers/JsonSocketServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Adventure.Core.Networking.Abstractions;
+
+namespace Adventure.Core.Networking.Providers
+{
+    public sealed class JsonSocketServer : SocketServer
+    {
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override event EventHandler<MessageReceivedArgs> OnMessageReceived;
+    }
+}
