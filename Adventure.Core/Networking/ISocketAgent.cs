42952f12338e5aee25728bf812e6e6d8692cf28c
Gino Messmer <git@ginomessmer.me>
2020-11-10 15:11:57 +0100

    Add base class for socket server

---
+++ Adventure.Core/Networking/ISocketAgent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Adventure.Core.Networking
+{
+    public interface ISocketAgent
+    {
+        void Start();
+
+        void Shutdown();
+
+        void SendMessage(string message);
+
+        event EventHandler<string> OnMessageReceived;
+    }
+}
--- Adventure.Core/Networking/SocketServer.cs
+++ Adventure.Core/Networking/SocketServer.cs
@@ -1,21 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Adventure.Core.Networking
 {
-    public abstract class SocketClient : ISocketAgent, IDisposable
+    public abstract class SocketServer : ISocketAgent
     {
         public abstract void Start();
 
         public abstract void SendMessage(string message);
 
-        public abstract void SendInitialMessage();
-
         public abstract event EventHandler<string> OnMessageReceived;
 
         public abstract void Shutdown();
-
-        public void Dispose() => Shutdown();
     }
 }
