c206687e676f8e5becce8a58bf1d4c57256a4cb6
Gino Messmer <git@ginomessmer.me>
2020-11-10 15:33:26 +0100

    Refactor server part

---
+++ Adventure.Core/Networking/SimpleSocketServer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Adventure.Core.Networking
+{
+    public sealed class SimpleSocketServer : SocketServer
+    {
+        private Socket _socket;
+        private byte[] _buffer;
+        private string _data;
+
+        public override void Start()
+        {
+            var builder = new SocketBuilder()
+                .WithHostEntry(SocketDefaults.LoopbackAddress)
+                .WithPort(SocketDefaults.Port);
+
+            _socket = builder.Build();
+            _socket.Bind(builder.Endpoint);
+            _socket.Listen(int.MaxValue);
+
+            _buffer = new byte[SocketDefaults.MessageSize];
+            _data = "";
+
+            while (true)
+            {
+                var receiveSocket = _socket.Accept();
+
+                while (receiveSocket.Connected)
+                {
+                    try
+                    {
+                        receiveSocket.Receive(_buffer);
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine("Socket exception: {0}", ex);
+                        break;
+                    }
+
+                    _data += Encoding.ASCII.GetString(_buffer);
+
+                    // Get header length value
+                    var headerIndex = _data.IndexOf(SocketDefaults.LengthHeaderName, StringComparison.Ordinal);
+                    if (headerIndex > -1)
+                    {
+                        var header = _data.Substring(headerIndex, SocketDefaults.HeaderSize);
+
+                        // Split or regex
+                        var headerKeyValue = header.Split(':');
+                        var length = Convert.ToInt32(headerKeyValue[1]);
+
+                        Console.WriteLine("Received new message");
+                        Console.WriteLine("- Length: {0}", length);
+
+                        var payload = _data.Substring(headerIndex + SocketDefaults.HeaderSize, length);
+
+                        Console.WriteLine("- Payload: {0}", payload);
+
+                        receiveSocket.Send(Encoding.ASCII.GetBytes("Great success"));
+                    }
+                }
+
+                _data = string.Empty;
+            }
+        }
+
+        public override void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override event EventHandler<string> OnMessageReceived;
+    }
+}
--- Adventure.Server/Program.cs
+++ Adventure.Server/Program.cs
@@ -13,64 +13,10 @@ namespace Adventure.Server
         public static async Task Main(string[] args)
         {
             // Create socket
-            Console.WriteLine("Opening socket...");
-            var entry = await Dns.GetHostEntryAsync("127.0.0.1");
-            var endpoint = new IPEndPoint(entry.AddressList.First(), SocketDefaults.Port);
-            var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Starting server");
 
-            // Bind
-            Console.WriteLine("BIND");
-            socket.Bind(endpoint);
-
-            // Listen
-            Console.WriteLine("LISTEN");
-            socket.Listen(int.MaxValue);
-            Console.WriteLine("Listening on {0}...", endpoint);
-
-            var buffer = new byte[SocketDefaults.MessageSize];
-            var data = "";
-
-            // Receive
-            while (true)
-            {
-                Console.WriteLine("ACCEPT...");
-                var receiveSocket = socket.Accept();
-
-                while (receiveSocket.Connected)
-                {
-                    try
-                    {
-                        receiveSocket.Receive(buffer);
-                    }
-                    catch (SocketException ex)
-                    {
-                        Console.WriteLine("Socket exception: {0}", ex);
-                        break;
-                    }
-
-                    data += Encoding.ASCII.GetString(buffer);
-
-                    // Get header length value
-                    var headerIndex = data.IndexOf(SocketDefaults.LengthHeaderName, StringComparison.Ordinal);
-                    if (headerIndex > -1)
-                    {
-                        var header = data.Substring(headerIndex, SocketDefaults.HeaderSize);
-
-                        // Split or regex
-                        var headerKeyValue = header.Split(':');
-                        var length = Convert.ToInt32(headerKeyValue[1]);
-
-                        Console.WriteLine("Received new message");
-                        Console.WriteLine("- Length: {0}", length);
-
-                        var payload = data.Substring(headerIndex + SocketDefaults.HeaderSize, length);
-
-                        Console.WriteLine("- Payload: {0}", payload);
-
-                        receiveSocket.Send(Encoding.ASCII.GetBytes("Great success"));
-                    }
-                }
-            }
+            var server = new SimpleSocketServer();
+            server.Start();
         }
     }
 }
