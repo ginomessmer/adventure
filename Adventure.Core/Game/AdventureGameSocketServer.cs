2c99108d7f8b7673a617ec20af56109e396f1a4f
Gino Messmer <gino@messmerized.de>
2020-11-20 13:35:52 +0100

    Add menu resources

--- Adventure.Core/Game/AdventureGameSocketServer.cs
+++ Adventure.Core/Game/AdventureGameSocketServer.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Adventure.Core.Commands;
 using Adventure.Core.Commands.Abstractions;
 using Adventure.Core.Domain;
+using Adventure.Core.Game.Resources;
 using Adventure.Core.Infrastructure;
 using Adventure.Core.Networking;
 using Adventure.Core.Networking.Abstractions;
@@ -32,6 +33,12 @@ namespace Adventure.Core.Game
                 args.ClientConnection.Id, args.ClientConnection.ClientEndpoint);
         }
 
+        /// <summary>
+        /// Processes incoming messages as JSON commands.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="connection"></param>
+        /// <returns></returns>
         private async Task HandleServerCommandMessageAsync(string message, SocketClientConnection connection)
         {
             var command = JsonConvert.DeserializeObject<ICommand>(message, JsonSocketDefaults.JsonSerializerSettings);
@@ -53,26 +60,50 @@ namespace Adventure.Core.Game
             }
         }
 
+        /// <summary>
+        /// Handler that executes when a client changes a scene.
+        /// </summary>
+        /// <param name="gameSession"></param>
+        /// <param name="scene"></param>
         private void GameOnSceneChanged(GameSession gameSession, Scene scene)
         {
             _logger.LogInformation("[{GameId}] SceneChanged: {SceneId}", gameSession.Id, scene.Id);
 
             var text = new StringBuilder()
                 .AppendLine(scene.Description)
-                .AppendLine("Was tust du?")
-                .AppendLine("Mögliche Aktionen: ")
-                .AppendJoin(',', scene.Actions.Select(x => x.Verb));
+                .AppendLine(MenuResources.CallToAction)
+                .AppendLine(MenuResources.AvailableActions)
+                .AppendJoin(',', scene.Actions.Select(x => $"{x.Verb} [ {string.Join(" | ", x.AllowedParameters)} ]"));
 
-            SendCommand(new PrintTextCommand(text.ToString()), GetClientConnection(gameSession).ClientSocket);
-            SendCommand(new PromptCommand(), GetClientConnection(gameSession).ClientSocket);
+            SendCommand(new PrintTextCommand(text.ToString()), GetClientConnection(gameSession));
+            SendCommand(new PromptCommand(), GetClientConnection(gameSession));
         }
 
+        /// <summary>
+        /// Sends a command to a socket receiver.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="receiver"></param>
         public void SendCommand(ICommand command, Socket receiver)
         {
             var json = JsonConvert.SerializeObject(command, JsonSocketDefaults.JsonSerializerSettings);
             SendMessage(json, receiver);
         }
 
+        /// <summary>
+        /// An alias to <seealso cref="SendCommand(Adventure.Core.Commands.Abstractions.ICommand,System.Net.Sockets.Socket)"/>.
+        /// <inheritdoc cref="SendCommand(Adventure.Core.Commands.Abstractions.ICommand,System.Net.Sockets.Socket)"/>
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="connection"></param>
+        public void SendCommand(ICommand command, SocketClientConnection connection) =>
+            SendCommand(command, connection.ClientSocket);
+
+        /// <summary>
+        /// Retrieves an existing client connection based on a game session.
+        /// </summary>
+        /// <param name="gameSession"></param>
+        /// <returns></returns>
         public SocketClientConnection GetClientConnection(GameSession gameSession) =>
             Connections.SingleOrDefault(x => x.Id == gameSession.Id);
     }
