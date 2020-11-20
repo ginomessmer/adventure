52c7d110a0fc8a74f7c6ceec4f8fa927b2211811
Gino Messmer <gino@messmerized.de>
2020-11-20 14:16:48 +0100

    Clean up client

--- Adventure.Client/Program.cs
+++ Adventure.Client/Program.cs
@@ -13,44 +13,5 @@ namespace Adventure.Client
 
             Console.ReadLine();
         }
-
-        private static void StartSimpleClient()
-        {
-            Console.WriteLine("Connecting to server...");
-            SocketClient client = new SimpleSocketClient();
-
-            client.OnConnected += (sender, eventArgs) => Console.WriteLine("Connected to server {0}", client.ServerEndPoint);
-
-            try
-            {
-                client.Start();
-            }
-            catch (Exception ex)
-            {
-                HandleExceptionAndShutdownGracefully(ex, "Error while connecting to the server");
-            }
-
-            try
-            {
-                Console.WriteLine("Send message...");
-                client.SendMessage("Hello world");
-                Console.WriteLine("Message sent");
-            }
-            catch (Exception ex)
-            {
-                HandleExceptionAndShutdownGracefully(ex, "Error while sending the message");
-            }
-
-            Console.WriteLine("Press any key to exit...");
-            Console.Read();
-        }
-
-        private static void HandleExceptionAndShutdownGracefully(Exception ex, string message = "An exception occurred")
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("{0}: {1}", message, ex);
-            Console.ReadLine();
-            Environment.Exit(1);
-        }
     }
 }
